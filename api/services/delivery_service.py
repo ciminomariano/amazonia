8e49746da105d15efa38246368860696b6ccb02f
cimin <ciminomariano@gmail.com>
2023-05-11 22:56:51 -0300

    Adding modifications to calculate the routes and time in service

--- api/services/delivery_service.py
+++ api/services/delivery_service.py
@@ -1,70 +1,97 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Any
 import requests
-
+import heapq
+import networkx as nx
 from api.models.models import DeliveryCreate, DeliveryOut, DeliveryInDB
 
 
-def get_all_possible_moves() -> Dict[str, Dict[str, float]]:
-    """Returns all possible moves between chessboard coordinates."""
-    res = requests.get("https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f")
-    return res.json()
+def get_time_between_coordinates():
+    api_url = "https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        G = nx.MultiDiGraph()
+
+        # Add edges with time as weight
+        for start, destinations in data.items():
+            for end, time in destinations.items():
+                G.add_edge(start, end, time=float(time))
+
+        return G
+    else:
+        # Manejar el error de alguna manera
+        pass
+
+
+def get_movement_time(start, end, graph):
+    # Check that the start and end points are valid
+    if start not in graph:
+        raise ValueError("Invalid start position")
+    if end not in graph:
+        raise ValueError("Invalid end position")
+
+    # Initialize the heap and visited set
+    heap = [(0, start)]
+    visited = set()
+
+    # Loop until the heap is empty
+    while heap:
+        # Pop the smallest cost point from the heap
+        current_cost, current_point = heapq.heappop(heap)
+
+        # Check if we've reached the end point
+        if current_point == end:
+            return current_cost
+
+        # Add the current point to the visited set
+        visited.add(current_point)
+
+        # Loop over the neighbors of the current point
+        for neighbor, cost in graph[current_point].items():
+            # Skip neighbors that have already been visited
+            if neighbor in visited:
+                continue
+
+            # Calculate the new cost to reach the neighbor
+            new_cost = current_cost + cost[0]["time"]
+
+            # Add the neighbor to the heap
+            heapq.heappush(heap, (new_cost, neighbor))
+
+    # If we reach here, there is no path between the start and end points
+    raise ValueError("No path between start and end points")
 
 
 from typing import Tuple
 
 
-def get_delivery_path(start: str, pickup: str, destination: str) -> Tuple[str, float]:
-    """
-    Returns the fastest delivery path and time elapsed from start to destination.
-    """
-    # Get all possible moves
-    all_moves = get_all_possible_moves()
+def get_delivery_path(start: str, pickup: str, destination: str) -> tuple[str, float]:
+    # Get the graph of travel times between positions
+    graph = get_time_between_coordinates()
 
-    # Dijkstra's shortest path algorithm
-    distances = {start: (None, 0)}
-    current_node = start
-    visited = set()
+    # Find shortest path from start to pickup
+    path_1 = nx.shortest_path(graph, start, pickup, weight='time')
 
-    while current_node != destination:
-        visited.add(current_node)
-        destinations = all_moves[current_node]
-        weight_to_current_node = distances[current_node][1]
+    # Find shortest path from pickup to destination
+    path_2 = nx.shortest_path(graph, pickup, destination, weight='time')
 
-        for next_node, weight in destinations.items():
-            if next_node not in visited:
-                weight = float(weight)
-                distance = weight_to_current_node + weight
-                if distances.get(next_node):
-                    if distances[next_node][1] > distance:
-                        distances[next_node] = (current_node, distance)
-                else:
-                    distances[next_node] = (current_node, distance)
+    # Combine the two paths into a single route
+    route = list(path_1) + path_2[1:]
 
-        next_destinations = {node: distance for node, distance in distances.items() if node not in visited}
-        if not next_destinations:
-            return None, None
+    # Calculate the time for each segment of the route
+    total_time = 0
+    route_times = []
+    for i in range(len(route) - 1):
+        start_pos = route[i]
+        end_pos = route[i + 1]
+        segment_time = get_movement_time(start_pos, end_pos, graph)
+        total_time += segment_time
+        route_times.append(segment_time)
 
-        # Next node is the destination with the lowest weight
-        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+    # Convert the route to a string
+    route_str = ",".join(route)
 
-    # Get the delivery path
-    path = []
-    while current_node is not None:
-        path.append(current_node)
-        next_node = distances[current_node][0]
-        current_node = next_node
-    path = path[::-1]
-
-    # Add the pickup location to the path
-    pickup_index = path.index(pickup)
-    path = path[:pickup_index + 1] + [f"P({pickup})"] + path[pickup_index + 1:]
-
-    # Calculate the time elapsed from start to destination
-    time_elapsed = distances[destination][1]
-
-    # Return the delivery path and time elapsed from start to destination
-    delivery_path = '-'.join(path)
-    return delivery_path, time_elapsed
+    return route_str, total_time
 
 
 def get_last_deliveries() -> List[DeliveryInDB]:
