77d157a79d2be0297e640e37d206ed96b61012bb
cimin <ciminomariano@gmail.com>
2023-05-14 00:00:16 -0300

    change in import

--- api/main.py
+++ api/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
-from endpoints.routes import router as delivery_router
+from fastapi.middleware.cors import CORSMiddleware
+from .endpoints.routes import router as delivery_router
 import uvicorn
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(delivery_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
-
 
