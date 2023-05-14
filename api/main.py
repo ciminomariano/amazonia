693cec55cdf4cdc563698eb3491e16850d8f7f88
cimin <ciminomariano@gmail.com>
2023-05-14 16:42:12 -0300

    modify main

--- api/main.py
+++ api/main.py
@@ -18,6 +18,5 @@ app.add_middleware(
 app.include_router(delivery_router)
 
 if __name__ == "__main__":
-
-    uvicorn.run("api.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("api.main:app", host="127.0.0.1", port=port, reload=True)
 
